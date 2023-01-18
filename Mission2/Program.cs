9c6cdec44747522613f983ea7f2e5d1fb818eb9e
DukeofNeptune <dallin.duke4@gmail.com>
2023-01-17 19:48:50 -0700

    Add project files.

---
+++ Mission2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Author: Dallin Duke
+// Description: Create a Dice game where you roll two die and show
+// the results in the form of asteriks that represent 1 percent of total number of rolls
+using System;
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating RollFunction
+            RollFunction rf = new RollFunction();
+
+            //Creating output and collecting input for the user for the dice game
+            Console.WriteLine("Welcome to the dice throwing simulator! \n");
+            Console.WriteLine("How many dice rolls would you like to simulate? ");
+            int numberofRolls  =  Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS\n");
+            Console.WriteLine("Each " + "'" + "*" + "'" + " represents 1% of the total number of rolls." +"\n");
+            Console.WriteLine("Total number of rolls = 1000.\n");
+            //Calling dice function
+            rf.DiceRollsCount(numberofRolls);
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
---
+++ Mission2/RollFunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2
+{
+    class RollFunction
+    {
+        public void DiceRollsCount(int NumRolls)
+        {
+            // Creating an array to hold dice tosses
+            int[] numbers = new int[13];
+            int randDieTotal = 0;
+            // Set Random up
+            Random rnd = new Random();
+
+            //Set Random dies up and add them to an array
+            for(int i = 0; i < NumRolls; i++)
+            {
+                int randDie1 = rnd.Next(1, 7);
+                int randDie2 = rnd.Next(1, 7);
+                randDieTotal = randDie1 + randDie2;
+
+                if(randDieTotal > 1 && randDieTotal < 13)
+                {
+                    numbers[randDieTotal]++;
+                }
+            }
+            //Print Results
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                int numResults = i;
+                double asteriks = Math.Round((float)numbers[i] / NumRolls * 100);
+                Console.Write(numResults + ": ");
+                for (int y = 0; y < asteriks; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+          
+        }
+    }
+}
