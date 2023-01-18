f80aad8515021d32d497b2a8eeabd112906122c5
DukeofNeptune <dallin.duke4@gmail.com>
2023-01-17 19:51:55 -0700

    removed spaces

--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -16,14 +16,11 @@ namespace Mission2
             Console.WriteLine("Welcome to the dice throwing simulator! \n");
             Console.WriteLine("How many dice rolls would you like to simulate? ");
             int numberofRolls  =  Convert.ToInt32(Console.ReadLine());
-
-            
-
-
             Console.WriteLine("\n");
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS\n");
             Console.WriteLine("Each " + "'" + "*" + "'" + " represents 1% of the total number of rolls." +"\n");
             Console.WriteLine("Total number of rolls = 1000.\n");
+
             //Calling dice function
             rf.DiceRollsCount(numberofRolls);
             Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
